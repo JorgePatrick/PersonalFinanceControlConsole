32905ef520be6b158bd966d7980d3c1d61fe0267
Jorge Patrick <jorgepatrickdev@gmail.com>
2021-04-10 18:25:39 -0300

    Project created

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PersonalFinanceControlConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
